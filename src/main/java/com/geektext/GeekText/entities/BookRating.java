8379f1d6d8fb5ec4a67a53d99f1aecbbdf858250
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2022-10-02 15:24:21 -0400

    use commentID as primary key for BookRating

--- src/main/java/com/geektext/GeekText/entities/BookRating.java
+++ src/main/java/com/geektext/GeekText/entities/BookRating.java
@@ -1,13 +1,19 @@
 package com.geektext.GeekText.entities;
 
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 
 @Entity
 @Table(name = "book_rating")
 @IdClass(BookRating.class)
-public class BookRating implements Serializable {
+public class BookRating implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer commentID;
 
     @Id
     @ManyToOne(targetEntity = Book.class, cascade = CascadeType.ALL)
