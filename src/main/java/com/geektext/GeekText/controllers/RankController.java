55892b95aff0a0a78d67ad7a75cdd5062b83c91b
Superstar64 <Hexagonalstar64@gmail.com>
2022-10-26 23:42:21 -0400

    delete dead code

--- src/main/java/com/geektext/GeekText/controllers/RankController.java
+++ src/main/java/com/geektext/GeekText/controllers/RankController.java
@@ -2,7 +2,6 @@ package com.geektext.GeekText.controllers;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
