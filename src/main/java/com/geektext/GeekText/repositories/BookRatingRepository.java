4b892609ba3855ab7817f5a60ba8678bdbf45c3a
Vanessa Chu <>
2022-11-09 12:45:49 -0500

    rank by book ratings and calc avg rating of each book

--- src/main/java/com/geektext/GeekText/repositories/BookRatingRepository.java
+++ src/main/java/com/geektext/GeekText/repositories/BookRatingRepository.java
@@ -1,8 +1,16 @@
 package com.geektext.GeekText.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.geektext.GeekText.entities.BookRating;
 
 public interface BookRatingRepository extends JpaRepository<BookRating, Integer> {
+    @Query("Select r from book_rating r where ?1 > 0 DESC")
+    List<BookRating> findByBookRatings(String isbn);
 
+    @Query("Select AVG(r.rating) from book_rating r where b.isbn.isbn = ?1")
+    List<BookRating> findByRatings(float rating);
 }
