a05299e233c6ae0eb09517886e51838b8d11e221
Superstar64 <Hexagonalstar64@gmail.com>
2022-11-27 00:24:47 -0500

    fix incorrect query in credit card

--- src/main/java/com/geektext/GeekText/repositories/CreditCardRepository.java
+++ src/main/java/com/geektext/GeekText/repositories/CreditCardRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CreditCardRepository extends JpaRepository<CreditCard, String> {
 
-    @Query("Select b FROM Person b WHERE b.username.username = ?1")
+    @Query("Select b FROM CreditCard b WHERE b.username.username = ?1")
     List<CreditCard> findByUsername(String username);
 
 }
